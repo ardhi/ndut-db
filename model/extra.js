3bc113bba261f6f280e1f6bf7a154036f05fe7cf
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-14 08:51:48 +0700

    Bug fix

--- model/extra.js
+++ model/extra.js
@@ -1,5 +1,6 @@
 module.exports = function (fastify, builder, model, schema) {
   const { _ } = fastify.ndut.helper
+  // TODO: can only be updated by Admins
   if (_.get(schema, 'feature.siteId')) builder.defineProperty(schema.name, 'siteId', {
     type: Number,
     required: false,
@@ -7,6 +8,7 @@ module.exports = function (fastify, builder, model, schema) {
     precision: 0,
     default: 0
   })
+  // TODO: can only be updated by Admins
   if (_.get(schema, 'feature.status')) builder.defineProperty(schema.name, 'status', {
     type: String,
     length: 10,
--- ndutRest/route/@model/get.js
+++ ndutRest/route/@model/get.js
@@ -13,35 +13,9 @@ module.exports = {
     }
   },
   handler: async function (request, reply) {
-    const { _, getNdutConfig } = this.ndut.helper
-    const restConfig = getNdutConfig(this, 'ndut-rest')
+    const { parseQsForList } = this.ndut.helper
     const model = this.ndutDb.helper.getModelByAlias(request.params.model)
-    let limit = parseInt(request.query[restConfig.queryKey.pageSize]) || restConfig.maxPageSize
-    if (limit > restConfig.maxPageSize) limit = restConfig.maxPageSize
-    if (limit < 1) limit = 1
-    let page = parseInt(request.query[restConfig.queryKey.page]) || 1
-    if (page < 1) page = 1
-    let skip = (page - 1) * limit
-    if (request.query[restConfig.queryKey.offset]) {
-      skip = parseInt(request.query[restConfig.queryKey.offset]) || skip
-      page = undefined
-    }
-    if (skip < 0) skip = 0
-    let where = {}
-    if (request.query[restConfig.queryKey.query]) {
-      try {
-        where = JSON.parse(request.query[restConfig.queryKey.query])
-      } catch (err) {
-        throw new Error(`Can't parse datasource query`)
-      }
-    }
-    let order = request.query[restConfig.queryKey.sort]
-    if (!order) {
-      const schema = _.find(getNdutConfig(this, 'ndut-db').schemas, { name: model }) || {}
-      const keys = _.map(schema.columnns, 'name')
-      const found = _.intersection(keys, ['updated_at', 'updatedAt', 'created_at', 'createdAt'])
-      if (found[0]) order = `${found[0]} DESC`
-    }
+    const { limit, page, skip, order, where } = parseQsForList(request, model)
     const total = await this.ndutDb.count(model, request, { where })
     const data = await this.ndutDb.find(model, request, { limit, order, skip, where })
     return {
