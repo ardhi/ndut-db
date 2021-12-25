39b754e505bdaa5c4608604cfa85ccf2c08de3eb
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-25 10:22:19 +0700

    Bug fix

---
+++ ndutRest/route/lookup/@model/@id/get.js
@@ -0,0 +1,28 @@
+module.exports = {
+  schema: {
+    description: 'Get record by its ID',
+    tags: ['DB'],
+    params: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'string',
+          description: 'Record ID'
+        }
+      }
+    }
+  },
+  handler: async function (request, reply) {
+    const { _ } = this.ndut.helper
+    const { getSchemaByAlias, getModelByAlias } = this.ndutDb.helper
+    const schema = getSchemaByAlias(request.params.model)
+    if (!schema.expose.get) throw this.Boom.notFound('Resource not found')
+    const model = getModelByAlias(request.params.model)
+    const base = 'DbLookup'
+    const data = await this.ndutDb.find(base, request, { where: { model, id: request.params.id } })
+    if (data.length === 0) throw this.Boom.notFound('Record not found')
+    return {
+      data: _.omit(data[0].toObject(), ['model'])
+    }
+  }
+}
---
+++ ndutRest/route/lookup/@model/get.js
@@ -0,0 +1,27 @@
+module.exports = {
+  schema: {
+    description: 'Get lookup\'s records',
+    tags: ['DB']
+  },
+  handler: async function (request, reply) {
+    const { _ } = this.ndut.helper
+    const { getSchemaByAlias, getModelByAlias } = this.ndutDb.helper
+    const { parseQsForList } = this.ndutRest.helper
+    const schema = getSchemaByAlias(request.params.model)
+    if (!schema.expose.list) throw this.Boom.notFound('Resource not found')
+    const model = getModelByAlias(request.params.model)
+    const base = 'DbLookup'
+    const { limit, page, skip, order, where } = parseQsForList(request, base)
+    where.model = model
+    const total = await this.ndutDb.count(base, request, where)
+    const result = await this.ndutDb.find(base, request, { limit, order, skip, where })
+    const data = _.map(result, d => _.omit(d.toObject(), ['model']))
+    return {
+      data,
+      total,
+      totalPage: Math.floor((total + limit - 1) / limit),
+      pageSize: limit,
+      page
+    }
+  }
+}
