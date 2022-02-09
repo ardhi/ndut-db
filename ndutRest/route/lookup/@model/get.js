941b6d6f32d25b1ca8d7b880de721622ef24020e
Ardhi Lukianto <ardhi@lukianto.com>
2022-02-09 21:03:11 +0700

    Bug fix & new feature

--- ndutRest/route/lookup/@model/get.js
+++ ndutRest/route/lookup/@model/get.js
@@ -13,7 +13,7 @@ module.exports = {
     const base = 'DbLookup'
     const { limit, page, skip, order, where } = await prepList(base, request.query)
     where.model = model
-    const total = await this.ndutApi.helper.count(base, where)
-    return await this.ndutApi.find(base,{ limit, order, skip, where, total })
+    const total = await this.ndutApi.helper.count({ model: base, params: { where } })
+    return await this.ndutApi.helper.find({ model: base, params: { limit, order, skip, where, total } })
   }
 }
