52a26361e8e8b78a2fea757f210cf73c0b5785d8
Ardhi Lukianto <ardhi@lukianto.com>
2022-10-19 15:41:49 +0700

    Updates

--- ndut/helper/import-fixture.js
+++ ndut/helper/import-fixture.js
@@ -1,10 +1,27 @@
 const path = require('path')
 
-const handler = async (records = [], { model, models, scope }) => {
+const handler = async (records = [], { model, models, transformer, scope }) => {
+  const { _ } = scope.ndut.helper
   // TODO: transaction, maybe?
-  for (const r of records) {
-    if (scope.ndutApi) await scope.ndutApi.helper.create({ model, body: r })
-    else await models[model].create(r)
+  let transAll
+  let transRec
+  if (transformer) {
+    if (_.isFunction(transformer)) transAll = transformer
+    else if (_.isPlainObject(transformer)) {
+      transAll = _.get(transformer, 'all')
+      transRec = _.get(transformer, 'record')
+    }
+  }
+  if (transAll) records = await transAll.call(scope, { model, models, data: records })
+  for (let r of records) {
+    try {
+      if (transRec) r = await transRec.call(scope, { model, models, data: records, record: r })
+      if (scope.ndutApi) await scope.ndutApi.helper.dbCall({ model, method: 'create', body: r })
+      else await models[model].create(r)
+    } catch (err) {
+      console.log(r)
+      throw err
+    }
   }
 }
 
@@ -20,20 +37,24 @@ module.exports = async function (model, silent, dir = 'fixture') {
   // builtin
   let files = await fastGlob(`${cfg.dir}/ndutDb/${dir}/${base}.{json,jsonl}`)
   let overridden = false
+  let transformer
+  try {
+    transformer = require(`${cfg.dir}/ndutDb/transformer/${base}.js`)
+  } catch (err) {}
   for (let f of files) {
     const overrides = await fastGlob(`${appCfg.dir}/ndutDb/${dir}/override/{${model},${_.kebabCase(model)}}.{json,jsonl}`)
     if (overrides.length > 0) {
       f = overrides[0]
       overridden = true
     }
-    await importFrom(f, handler, { handler: { model, models, scope: this } })
+    await importFrom(f, handler, { handler: { model, models, transformer, scope: this } })
     if (!silent) this.log.debug(`* Builtin ${dir} '${path.basename(f)}' loaded successfully`)
   }
   if (overridden) return
   // additional
   files = await fastGlob(`${appCfg.dir}/ndutDb/${dir}/extend/{${model},${_.kebabCase(model)}}.{json,jsonl}`)
   for (const f of files) {
-    await importFrom(f, handler, { handler: { model, models, scope: this } })
+    await importFrom(f, handler, { handler: { model, models, transformer, scope: this } })
     if (!silent) this.log.debug(`* ${_.upperFirst(dir)} '${path.basename(f)}' loaded successfully`)
   }
 }
